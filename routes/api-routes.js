39d5f15f3a7c727d5d1c1e013f3920705c12523d
rpc08002 <rcurtin0491@gmail.com>
2021-03-09 16:13:18 -0500

    created api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/workouts/:id", ({ params, body }, res) => {
+  console.log(body);
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: {
+        exercises: body,
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/workouts", ({ body }, res) => {
+  console.log(body);
+  db.Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+
+  res.send("Got a POST request");
+});
+
+router.get("/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
